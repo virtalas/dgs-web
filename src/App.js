0118ea796654b8ff9d2e36fb006edc8387885362
virtalas <virtalas@users.noreply.github.com>
2019-02-18 16:51:23 +0200

    Added responsive drawer for navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import Typography from '@material-ui/core/Typography'
+import Drawer from './components/Drawer'
+
+import './css/App.css'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Drawer />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
