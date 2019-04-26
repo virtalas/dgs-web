2ebf748fec1d1ee45200b7daaed55292a91c5262
virtalas <virtalas@users.noreply.github.com>
2019-04-26 11:36:00 +0300

    Use ThemeProvider as temporary solution until Material UI v4, use Typography v2, menu popup for ScoreInputPage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react'
 import Router from './components/Router'
 
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import ThemeProvider from "@material-ui/styles/ThemeProvider";
+
 import './css/App.css'
+
+// Temporary solution for using @material-ui/styles (will be unneeded in Material UI v4):
+// https://github.com/mui-org/material-ui/issues/14271
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true, // Use new typography v2
+  },
+});
 
 // TODO: handle login, pass signed in player info as props
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <ThemeProvider theme={theme}>
         <Router />
-      </div>
+      </ThemeProvider>
     )
   }
 }
--- src/components/games/ScoreInputPage.js
+++ src/components/games/ScoreInputPage.js
@@ -6,20 +6,33 @@ import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
+    // flexGrow: 1,
   },
   menuButton: {
+    marginRight: 0
   },
   par: {
-    flexGrow: 1,
+    // flexGrow: 1,
+    margin: 'auto'
   },
 }))
 
 function ScoreInputPage() {
   const classes = useStyles()
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  function handleClick(event) {
+    setAnchorEl(event.currentTarget);
+  }
+
+  function handleClose() {
+    setAnchorEl(null);
+  }
   return (
     <div className={classes.root}>
       <AppBar position="fixed">
@@ -30,9 +43,19 @@ function ScoreInputPage() {
           <Typography variant="h6" color="inherit" className={classes.par}>
             Par X
           </Typography>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
+          <Button
+            aria-owns={anchorEl ? 'simple-menu' : undefined}
+            aria-haspopup="true"
+            onClick={handleClick}
+            className={classes.menuButton}
+          >
+            Open Menu
+          </Button>
+          <Menu id="simple-menu" anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
+            <MenuItem onClick={handleClose}>Profile</MenuItem>
+            <MenuItem onClick={handleClose}>My account</MenuItem>
+            <MenuItem onClick={handleClose}>Logout</MenuItem>
+          </Menu>
         </Toolbar>
       </AppBar>
     </div>
