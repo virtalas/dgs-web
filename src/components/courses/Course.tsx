80c30902abf8b304ea6dc827220bc78c6879b62e
virtalas <lasvirt@gmail.com>
2022-08-08 16:40:14 +0300

    Use bigger icon resolution for course page

--- src/components/courses/Course.tsx
+++ src/components/courses/Course.tsx
@@ -251,7 +251,7 @@ const Course: React.FC<Props> = (props) => {
         <div className={classes.weatherFlexbox}>
           <img
             className={classes.weatherImage}
-            src={`https://openweathermap.org/img/wn/${localWeather.iconId}.png`}
+            src={`https://openweathermap.org/img/wn/${localWeather.iconId}@2x.png`}
             alt="Current weather on the course"
           />
 
